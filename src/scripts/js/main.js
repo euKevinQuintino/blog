f40482de465984233a53369cbc4c309d66e937b8
KEVIN NICOLAS BUENO QUINTINO <kevin.quintino@fatec.sp.gov.br>
2021-05-15 14:40:26 -0300

    0.0.9

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -2,22 +2,12 @@ function toStart() {
   window.location.reload();
 }
 
-function changeCategory(category) {
-  if (category == "esports") {
-    document.querySelector(".design").style.display = "none";
-    document.querySelector(".esports").style.display = "block";
-  } else if (category == "design") {
-    document.querySelector(".esports").style.display = "none";
-    document.querySelector(".design").style.display = "block";
-  }
-}
-
 //gsap
-gsap.from(".blog__homepage__card-section", {
+/*gsap.from(".blog__homepage__card-section", {
   scrollTrigger: ".blog__homepage__card-section",
   start: "top center",
   opacity: 0,
   y: 100,
   duration: 0.69,
   delay: 0.69,
-});
+});*/
